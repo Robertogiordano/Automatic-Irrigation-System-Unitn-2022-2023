bc6d89ebb3e9d65acbf7cbb4d91d65b4d903cd73
Robertogiordano <107034283+Robertogiordano@users.noreply.github.com>
2023-01-26 16:23:31 +0100

    Add files via upload

---
+++ lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.d
@@ -0,0 +1,162 @@
+# FIXED
+
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: ../lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: ../lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/driverlib.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/adc14.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/aes256.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/comp_e.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cpu.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/crc32.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cs.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/dma.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/interrupt.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/eusci.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/fpu.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/gpio.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/i2c.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/mpu.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pcm.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pss.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ref_a.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/reset.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom_map.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rtc_c.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/spi.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/systick.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer32.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/uart.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/wdt_a.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/sysctl.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/flash.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/grlib.h
+lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj: /home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/assert.h
+
+../lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c:
+
+../lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/driverlib.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/adc14.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+/home/student/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+/home/student/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+/home/student/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/aes256.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/comp_e.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cpu.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/crc32.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cs.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/dma.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/interrupt.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/eusci.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/fpu.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/gpio.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/i2c.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/mpu.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pcm.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pss.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ref_a.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/reset.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom_map.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rtc_c.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/spi.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/systick.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer32.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/uart.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/wdt_a.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/sysctl.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/flash.h:
+
+/home/student/Downloads/simplelink_msp432p4_sdk_3_40_01_02/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/grlib.h:
+
+/home/student/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/assert.h:
+
---
+++ lib/LcdDriver/subdir_vars.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../lib/LcdDriver/Crystalfontz128x128_ST7735.c \
+../lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c 
+
+C_DEPS += \
+./lib/LcdDriver/Crystalfontz128x128_ST7735.d \
+./lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.d 
+
+OBJS += \
+./lib/LcdDriver/Crystalfontz128x128_ST7735.obj \
+./lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj 
+
+OBJS__QUOTED += \
+"lib/LcdDriver/Crystalfontz128x128_ST7735.obj" \
+"lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.obj" 
+
+C_DEPS__QUOTED += \
+"lib/LcdDriver/Crystalfontz128x128_ST7735.d" \
+"lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.d" 
+
+C_SRCS__QUOTED += \
+"../lib/LcdDriver/Crystalfontz128x128_ST7735.c" \
+"../lib/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c" 
+
+
